1405e87a158e3ff38a7b7f16fe310ba01ac57898
john6604 <jchimbo456@gmail.com>
2026-06-16 09:56:01 -0500

    feat: Construction method for documents and random UUID

--- internal/document/document.go
+++ internal/document/document.go
@@ -1,8 +1,15 @@
 package document
 
+import (
+	"crypto/rand"
+	"fmt"
+)
+
+// Package variables for Start and End nodes
 var startID = NewID("START", -1)
 var endID = NewID("END", -2)
 
+// Definition os structure
 type Document struct {
 	Start            *Element
 	End              *Element
@@ -10,4 +17,42 @@ type Document struct {
 	CharacterCounter int
 	clientID         string
 	clock            int
+}
+
+// Generate a random UUID
+func generateUUID() string {
+	b := make([]byte, 16)
+	_, err := rand.Read(b)
+	if err != nil {
+		return err.Error()
+	}
+
+	uuid := fmt.Sprintf("%x-%x-%x-%x-%x",
+		b[0:4], b[4:6], b[6:8], b[8:10], b[10:])
+	return uuid
+}
+
+// Generate a new document
+func NewDocument() *Document {
+
+	//Document attributes defined
+	document := Document{CharacterCounter: 0}
+	start := NewElement(*startID, nil, nil, nil, '\x00')
+	end := NewElement(*endID, nil, nil, nil, '\x00')
+	document.ElementsByID = make(map[ID]*Element)
+	document.CharacterCounter = 0
+	document.clientID = generateUUID()
+	document.clock = 0
+
+	//Initial logic defined
+	start.Right = end
+	end.Left = start
+	document.Start = start
+	document.End = end
+
+	//Initialize linked list
+	document.ElementsByID[start.ElementID] = start
+	document.ElementsByID[end.ElementID] = end
+
+	return &document
 }
