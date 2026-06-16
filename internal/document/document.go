7cc33a0495bf9d16a77a10f31266245090c7e5d8
john6604 <jchimbo456@gmail.com>
2026-06-16 09:21:47 -0500

    feat: document structure declared

--- internal/document/document.go
+++ internal/document/document.go
@@ -1,1 +1,13 @@
 package document
+
+var startID = NewID("START", -1)
+var endID = NewID("END", -2)
+
+type Document struct {
+	Start            *Element
+	End              *Element
+	ElementsByID     map[ID]*Element
+	CharacterCounter int
+	clientID         string
+	clock            int
+}
