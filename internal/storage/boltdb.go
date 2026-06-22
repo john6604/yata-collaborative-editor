95f042966083fb3dacf1fa7eca5298bdd514f073
john6604 <jchimbo456@gmail.com>
2026-06-22 17:59:53 -0500

    feat: opening and closing DB, separated roles

--- internal/storage/boltdb.go
+++ internal/storage/boltdb.go
@@ -12,7 +12,7 @@ type Storage struct {
 }
 
 func (s *Storage) OpenDB() error {
-	db, err := bolt.Open("yata.db", 0o600, &bolt.Options{Timeout: 2 * time.Second})
+	db, err := bolt.Open("..\\..\\internal\\storage\\yata.db", 0o600, &bolt.Options{Timeout: 2 * time.Second})
 
 	if err != nil {
 		return err
@@ -20,27 +20,32 @@ func (s *Storage) OpenDB() error {
 
 	s.db = db
 
-	s.db.Update(func(tx *bolt.Tx) error {
-		_, err := tx.CreateBucketIfNotExists([]byte("metadata"))
+	errBuckets := s.db.Update(func(tx *bolt.Tx) error {
+		_, err1 := tx.CreateBucketIfNotExists([]byte("metadata"))
 
-		if err != nil {
-			return fmt.Errorf("create bucket: %s", err)
+		if err1 != nil {
+			return fmt.Errorf("create bucket: %s", err1)
+		}
+
+		_, err2 := tx.CreateBucketIfNotExists([]byte("elements"))
+		if err2 != nil {
+			return fmt.Errorf("create bucket: %s", err2)
 		}
 
 		return nil
 	})
 
-	s.db.Update(func(tx *bolt.Tx) error {
-		_, err := tx.CreateBucketIfNotExists([]byte("elements"))
+	if errBuckets != nil {
+		return errBuckets
+	}
 
-		if err != nil {
-			return fmt.Errorf("create bucket: %s", err)
-		}
+	return nil
+}
 
-		return nil
-	})
-
-	defer s.db.Close()
+func (s *Storage) CloseDB() error {
+	if err := s.db.Close(); err != nil {
+		return err
+	}
 
 	return nil
 }
